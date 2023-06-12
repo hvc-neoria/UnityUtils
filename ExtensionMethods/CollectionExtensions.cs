2dd60af25679c3a42f0b7e85c989a94082e73620
neoria <hvcharu@gmail.com>
2023-06-13 08:32:07 +0900

    シャッフルメソッドの引数のコレクションを加工しない形に修正

--- ExtensionMethods/CollectionExtensions.cs
+++ ExtensionMethods/CollectionExtensions.cs
@@ -15,18 +15,24 @@ namespace HvcNeoria.Unity.Utils
         /// フィッシャー–イェーツのシャッフル。改良バージョン。
         /// 参考：https://ja.wikipedia.org/wiki/%E3%83%95%E3%82%A3%E3%83%83%E3%82%B7%E3%83%A3%E3%83%BC%E2%80%93%E3%82%A4%E3%82%A7%E3%83%BC%E3%83%84%E3%81%AE%E3%82%B7%E3%83%A3%E3%83%83%E3%83%95%E3%83%AB
         /// </summary>
-        /// <param name="iList">リスト</param>
-        public static void Shuffle(this IList iList)
+        /// <param name="iEnumerable">コレクション</param>
+        /// <typeparam name="T">コレクションの要素の型</typeparam>
+        /// <returns>シャッフルされた配列</returns>
+        public static T[] Shuffle<T>(this IEnumerable<T> iEnumerable)
         {
-            for (int i = 0; i < iList.Count - 2; i++)
+            var array = iEnumerable.ToArray();
+
+            for (int i = 0; i < array.Length - 2; i++)
             {
-                var j = Random.Range(i, iList.Count);
+                var j = Random.Range(i, array.Length);
 
                 // i番目とj番目の要素を交換する
-                var tmp = iList[i];
-                iList[i] = iList[j];
-                iList[j] = tmp;
+                var tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
             }
+
+            return array;
         }
 
         /// <summary>
@@ -34,9 +40,9 @@ namespace HvcNeoria.Unity.Utils
         /// floatの同値判定は性質上うまくいかないことが多いので、
         /// ほぼ同じ値であることを判定するMathf.Approximately()を使用している。
         /// </summary>
-        /// <param name="floats">floatのiList</param>
-        /// <returns>重複削除したfloatのList</returns>
-        public static List<float> Distinct(this IList<float> floats)
+        /// <param name="floats">floatのIEnumerable</param>
+        /// <returns>重複削除されたfloatのList</returns>
+        public static List<float> Distinct(this IEnumerable<float> floats)
         {
             var result = new List<float>();
 
