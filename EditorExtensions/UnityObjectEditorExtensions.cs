7d23aaf55ce285b2b933f64604f09452fcd21a72
neoria <hvcharu@gmail.com>
2023-09-08 06:30:54 +0900

    サブアセット取得メソッドを追加

---
+++ EditorExtensions/UnityObjectEditorExtensions.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using UnityEditor;
+
+namespace HvcNeoria.Unity.Utils
+{
+    public static class UnityObjectEditorExtensions
+    {
+        /// <summary>
+        /// 指定したUnityEngine.Objectのサブアセットを取得する。
+        /// </summary>
+        /// <param name="obj">UnityEngineのObject</param>
+        /// <returns>サブアセット</returns>
+        public static UnityEngine.Object[] GetSubAssets(this UnityEngine.Object obj)
+        {
+            var path = AssetDatabase.GetAssetPath(obj);
+            var assets = AssetDatabase.LoadAllAssetsAtPath(path);
+            return assets.Where(x => AssetDatabase.IsSubAsset(x)).ToArray();
+        }
+    }
+}
