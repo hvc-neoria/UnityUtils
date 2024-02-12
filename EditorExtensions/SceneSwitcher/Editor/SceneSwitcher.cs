72360c46ed342e5610673557d5cc0098cd2eb55e
neoria <hvcharu@gmail.com>
2024-02-12 11:04:34 +0900

    SceneSwitcherを本ネームスペースに含めるよう修正

--- EditorExtensions/SceneSwitcher/Editor/SceneSwitcher.cs
+++ EditorExtensions/SceneSwitcher/Editor/SceneSwitcher.cs
@@ -2,29 +2,40 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
-public class SceneSwitcher : EditorWindow
+namespace HvcNeoria.Unity.Utils
 {
-    /// <Summary>
-    /// ウィンドウを表示します。
-    /// </Summary>
-    [MenuItem("Tools/Scene Switcher")]
-    static void Open()
-    {
-        var window = GetWindow<SceneSwitcher>();
-        window.titleContent = new GUIContent("Scene Switcher");
-    }
-
     /// <summary>
-    /// Scene In Build のシーンをボタンで表示します。
+    /// Unityの上部メニューバーの Tools に Scene Switcher を追加します。
     /// </summary>
-    void OnGUI()
+    /// <remarks>
+    /// 使い方
+    /// ・Unityの上部メニューバーの Tools > Scene Switcher をクリックする
+    /// ・切り替えたいシーンのボタンをクリックする
+    /// </remarks>
+    public class SceneSwitcher : EditorWindow
     {
-        for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
+        /// <Summary>
+        /// シーン切り替えウィンドウを表示します。
+        /// </Summary>
+        [MenuItem("Tools/Scene Switcher")]
+        static void Open()
         {
-            if (GUILayout.Button(EditorBuildSettings.scenes[i].path.Replace("Assets/", "").Replace(".unity", "")))
+            var window = GetWindow<SceneSwitcher>();
+            window.titleContent = new GUIContent("Scene Switcher");
+        }
+
+        /// <summary>
+        /// Scene In Build に登録されているシーンをボタンとして表示します。
+        /// </summary>
+        void OnGUI()
+        {
+            for (int i = 0; i < EditorBuildSettings.scenes.Length; i++)
             {
-                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
-                EditorSceneManager.OpenScene(EditorBuildSettings.scenes[i].path);
+                if (GUILayout.Button(EditorBuildSettings.scenes[i].path.Replace("Assets/", "").Replace(".unity", "")))
+                {
+                    EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                    EditorSceneManager.OpenScene(EditorBuildSettings.scenes[i].path);
+                }
             }
         }
     }
