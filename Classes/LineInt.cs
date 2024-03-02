e66a50ac1a0bf021f3165b5b20037d553de7eeab
neoria <hvcharu@gmail.com>
2024-03-03 08:31:10 +0900

    LineInt追加

---
+++ Classes/LineInt.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+namespace HvcNeoria.Unity.Utils
+{
+    /// <summary>
+    /// 線分。
+    /// </summary>
+    public struct LineInt : IRectangleInt
+    {
+        /// <summary>
+        /// 左下隅の位置。
+        /// </summary>
+        public Vector2Int Position { get; }
+        public Vector2Int Size { get; }
+        public Vector2Int LocalPosition { get; }
+
+        public int Left => Position.x;
+        public int Right => Position.x + Size.x - 1;
+        public int Bottom => Position.y;
+        public int Top => Position.y + Size.y - 1;
+
+        public Vector2Int LeftBottom => new Vector2Int(Left, Bottom);
+        public Vector2Int RightBottom => new Vector2Int(Right, Bottom);
+        public Vector2Int LeftTop => new Vector2Int(Left, Top);
+        public Vector2Int RightTop => new Vector2Int(Right, Top);
+
+        public int LocalLeft => LocalPosition.x;
+        public int LocalRight => LocalPosition.x + Size.x - 1;
+        public int LocalBottom => LocalPosition.y;
+        public int LocalTop => LocalPosition.y + Size.y - 1;
+
+        public int Width => Size.x;
+        public int Height => Size.y;
+
+        public int Area => Size.x * Size.y;
+
+        public LineInt(Vector2Int position, Vector2Int size, Vector2Int parentPosition = default(Vector2Int))
+        {
+            if (size.x != 1 && size.y != 1)
+            {
+                throw new ArgumentException("幅も高さも1ではありません。");
+            }
+
+            Position = position;
+            Size = size;
+            LocalPosition = position - parentPosition;
+        }
+    }
+}
